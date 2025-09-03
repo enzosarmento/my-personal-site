ad428143f7a4fe1221acdf1cec4ef3c11acd273f
enzosarmento <enzosarmento91@gmail.com>
2025-09-03 14:00:41 -0300

    refactor: configuration github pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/enzosarmento.github.io'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
